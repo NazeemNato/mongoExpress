68e934f3cf485fc3c438c23b000c0ab072ec516a
Muhammed Nazeem <39720973+NazeemNato@users.noreply.github.com>
2020-08-05 23:09:23 +0530

    models

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema({
+	username: {
+		type:String,
+		require: true
+	},
+	email: {
+		type:String,
+		require: true
+	},
+	date: {
+		type:Date,
+		default: Date.now
+	}
+})
+
+
+module.exports = mongoose.model('Users',UserSchema)
